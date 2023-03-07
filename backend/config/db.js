395f36a38cc8748e7f7e04be3a3f4ff082ac864a
Kirubel <kirubel@ienetworks.co>
2023-03-07 20:38:21 +0300

    secrete fix on mongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const ConnectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://kirubels:kirubels@webapp.xx245vp.mongodb.net/addisplay?retryWrites=true&w=majority')
+        const conn = await mongoose.connect(process.env.MONGO_URI)
 
         console.log(`DB connected ${conn.connection.host}`)
     } catch (error) {
