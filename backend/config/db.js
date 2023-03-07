b34503ec0a9f8857737eeaec7062cacd1ade12d3
KIRUBEL SIMACHEW <75899792+Kirubel25@users.noreply.github.com>
2023-03-07 22:23:54 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 
 const ConnectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb+srv://kirubels:kirubels@webapp.xx245vp.mongodb.net/addisplay?retryWrites=true&w=majority")
 
         console.log(`DB connected ${conn.connection.host}`)
     } catch (error) {
